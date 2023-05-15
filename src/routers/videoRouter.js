0c5572a1268d15dc4307d05ef7c0e23cda1861d6
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-05-15 09:25:23 +0900

    planning routes

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { watch, edit } from "../controllers/videoController";
+import {
+  watch,
+  edit,
+  upload,
+  deleteVideo,
+} from "../controllers/videoController";
+import { see } from "../controllers/userController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watch);
-videoRouter.get("/edit", edit);
+videoRouter.get("/upload", upload);
+videoRouter.get("/:id", see);
+videoRouter.get("/:id/edit", edit);
+videoRouter.get("/:id/delete", deleteVideo);
 
 export default videoRouter;
