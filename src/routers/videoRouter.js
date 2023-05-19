75f5aa248a9625ff1e57c3fd8ecdb8c58bcc2813
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-05-19 12:03:00 +0900

    3.8 url parameters part two

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,17 +1,11 @@
 import express from "express";
-import {
-  watch,
-  edit,
-  upload,
-  deleteVideo,
-} from "../controllers/videoController";
-import { see } from "../controllers/userController";
+import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
 
 export default videoRouter;
