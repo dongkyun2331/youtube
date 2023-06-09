6e7e81b4cfb99142efba817052908329200bdecb
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-06-09 13:22:20 +0900

    5.0 Array Database part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,5 @@
 export const trending = (req, res) => {
-  const videos = [
+  let videos = [
     {
       title: "First Video",
       rating: 5,
@@ -14,7 +14,7 @@ export const trending = (req, res) => {
       comments: 2,
       createdAt: "2 minutes ago",
       views: 59,
-      id: 1,
+      id: 2,
     },
     {
       title: "Third Video",
@@ -22,15 +22,17 @@ export const trending = (req, res) => {
       comments: 2,
       createdAt: "2 minutes ago",
       views: 59,
-      id: 1,
+      id: 3,
     },
   ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch");
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching &{video.title}` });
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.render("Search");
 export const upload = (req, res) => res.render("Upload");
-export const deleteVideo = (req, res) => {
-  return res.render("Delete Video");
-};
+export const deleteVideo = (req, res) => res.render("Delete Video");
