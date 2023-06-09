dec6111598cd006f6ea08271d001cf5d5a9be89c
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-06-09 14:14:49 +0900

    5.3 Edit Video part Two

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -10,7 +10,6 @@ import {
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", getedit);
-videoRouter.post("/:id(\\d+)/edit", postEdit);
+videoRouter.route("/:id(\\d+)/edit").get(getedit).post(postEdit);
 
 export default videoRouter;
