26cc456f4d97bf8707bbd8becb940e69f949baab
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-11-18 16:51:32 +0900

    new videos

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -21,6 +21,18 @@ const Feed = () => {
           Copyright 2023
         </Typography>
       </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          새로운 <span style={{ color: "#F31503" }}>영상</span>
+        </Typography>
+        <Videos />
+      </Box>
     </Stack>
   );
 };
