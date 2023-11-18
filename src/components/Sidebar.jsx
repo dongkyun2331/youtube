d0ce0652fb9d30878f5baac279174a843a2e9aba
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-11-18 15:58:15 +0900

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,8 @@
 import { Stack } from "@mui/material";
+
 import { categories } from "../utils/constants";
-import { Category } from "@mui/icons-material";
+
+const selectedCategory = "New";
 
 const Sidebar = () => (
   <Stack
@@ -11,10 +13,28 @@ const Sidebar = () => (
       flexDirection: { md: "column" },
     }}
   >
-    {categories.map((Category) => (
-      <button>
-        <span>{Category.icon}</span>
-        <span>{Category.name}</span>
+    {categories.map((category) => (
+      <button
+        className="category-btn"
+        style={{
+          background: category.name === selectedCategory && "#FC1503",
+          color: "white",
+        }}
+        key={category.name}
+      >
+        <span
+          style={{
+            color: category.name === selectedCategory ? "white" : "red",
+            marginRight: "15px",
+          }}
+        >
+          {category.icon}
+        </span>
+        <span
+          style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}
+        >
+          {category.name}
+        </span>
       </button>
     ))}
   </Stack>
