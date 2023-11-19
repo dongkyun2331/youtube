313359049541cdedbd86105d2f1901c13b970ec9
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-11-20 02:30:50 +0900

    BASE_URL API Key

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,16 +1,14 @@
 import axios from "axios";
 
+const BASE_URL = "https://youtube-v31.p.rapidapi.com/search";
+
 const options = {
-  method: "GET",
-  url: "https://youtube-v31.p.rapidapi.com/search",
+  url: BASE_URL,
   params: {
-    relatedToVideoId: "7ghhRHRP6t4",
-    part: "id,snippet",
-    type: "video",
     maxResults: "50",
   },
   headers: {
-    "X-RapidAPI-Key": "f75756d68amsh3a59e6682eefb94p185439jsn2751f77d1b72",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
