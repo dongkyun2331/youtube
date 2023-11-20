64919253507bead7f567a4f3382f7508596c6547
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-11-20 14:17:39 +0900

    response

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const BASE_URL = "https://youtube-v31.p.rapidapi.com/search";
 
 const options = {
-  url: BASE_URL,
   params: {
     maxResults: "50",
   },
@@ -11,4 +10,8 @@ const options = {
     "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
+};
+
+export const fetchFromAPI = async (url) => {
+  const response = await axios.get(`${BASE_URL}/${url}`, options);
 };
