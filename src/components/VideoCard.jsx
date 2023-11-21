6f31f062acb6a866e29b126349ccc2bd8284a077
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-11-22 03:07:09 +0900

    VideoCard console.log video

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
 
-const VideoCard = () => {
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from "../utils/constants";
+
+const VideoCard = ({ video }) => {
+  console.log(video);
+
   return <div>VideoCard</div>;
 };
 
