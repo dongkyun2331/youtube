1c92abeea73eb4ad0f99ad6cb58b75d9b8396213
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-11-22 18:31:37 +0900

    CheckCircle

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -38,8 +38,9 @@ const VideoCard = ({
               : demoChannelUrl
           }
         >
-          <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+          <Typography variant="subtitle2" fontWeight="bold" color="gray">
             {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle sx={{ fontSize: 12, color: "gray", ml: "5px" }} />
           </Typography>
         </Link>
       </CardContent>
