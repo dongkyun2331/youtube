71750d6db1a131d86bb31f927bd712091ae97a12
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-11-30 19:29:31 +0900

    videos

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -4,16 +4,21 @@ import ReactPlayer from "react-player";
 import { Typography, Box, Stack } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
 
-import { Video } from "./";
+import { Videos } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
       setVideoDetail(data.items[0])
+    );
+
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
+      (data) => setVideos(data.items)
     );
   }, [id]);
 
@@ -57,7 +62,7 @@ const VideoDetail = () => {
                   />
                 </Typography>
               </Link>
-              <Stack>
+              <Stack direction="row" gap="20px" alignItems="center">
                 <Typography variant="body1" sx={{ opacity: 0.7 }}>
                   {parseInt(viewCount).toLocaleString()} views
                 </Typography>
@@ -69,6 +74,15 @@ const VideoDetail = () => {
           </Box>
         </Box>
       </Stack>
+
+      <Box
+        px={2}
+        py={{ md: 1, xs: 5 }}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Videos videos={videos} />
+      </Box>
     </Box>
   );
 };
