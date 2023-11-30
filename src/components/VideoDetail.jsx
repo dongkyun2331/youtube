976d9a3ba69c92fed2cc1cbb6a9ca2f0098f64eb
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-11-30 16:52:23 +0900

    VideoDetail ReactPlayer

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -8,7 +8,25 @@ import { Video } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
-  return <Box minHeight="95vh"></Box>;
+  const [videoDetail, setVideoDetail] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
+      setVideoDetail(data.items[0])
+    );
+  }, [id]);
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
+  );
 };
 
 export default VideoDetail;
