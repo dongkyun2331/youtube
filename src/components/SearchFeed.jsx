69cd9d2f9fc5da3efc7ff69a56270cdb3a79efc4
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-11-30 15:49:38 +0900

    VideoDetail import

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -13,7 +13,7 @@ const SearchFeed = () => {
     fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
       setVideos(data.items)
     );
-  }, []);
+  }, [searchTerm]);
 
   return (
     <Box p={2} minHeight="95vh">
